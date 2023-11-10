afef260a68b135e1c4ebde7ecafc0184a8534531
bingyanghuang0217 <91875227+bingyanghuang0217@users.noreply.github.com>
2023-11-10 21:10:32 +0000

    1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,8 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  
   def director
     my_director_id = self.director_id
 
@@ -23,3 +25,4 @@ class Movie < ApplicationRecord
     return the_director
   end
 end
+
